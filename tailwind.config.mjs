b4225438417fe879f1b281b33eefe13a1a8658fd
matprestes <git@stackblitz.com>
2024-11-16 23:57:17 -0300

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1a5336',
+        secondary: '#2d7a4f'
+      }
+    },
+  },
+  plugins: [],
+}
